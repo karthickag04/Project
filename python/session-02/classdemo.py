f3246a398b6ccfb9e0b0a47c31ddcebe8d068d47
karthickag04 <karthickag04@gmail.com>
2024-02-09 14:17:33 +0530

    updated session02 files in python

---
+++ python/session-02/classdemo.py
@@ -0,0 +1,27 @@
+# class grandparents:
+#     def __init__(self,grandpaname, grandmaname, familyname) :
+#         self.grandfathername=grandpaname
+#         self.grandmothername=grandmaname
+#         self.familyname=familyname
+
+#     def welcome(self):
+#         print(self.grandfathername,self.grandmothername, self.familyname)
+    
+        
+
+# x=grandparents("kalaingar","Dhayalu ammal","DMK")
+# x.welcome()
+
+class grandparents:
+    def __init__(self,grandpaname, grandmaname, familyname) :
+        self.grandfathername=grandpaname
+        self.grandmothername=grandmaname
+        self.familyname=familyname
+
+    def welcome(self):
+        print(self.grandfathername,self.grandmothername, self.familyname)
+    
+        
+
+x=grandparents("kalaingar","Dhayalu ammal","DMK")
+x.welcome()
---
+++ python/session-02/funtiondemo.py
@@ -0,0 +1,61 @@
+# #addressing space issue --> use  tab key in keyboard
+# def myname():
+#     print("this is kathick ag")
+#     print("welcome")
+#     name="karthick1"
+#     lname="AG"
+#     if name=="karthick":
+#         print ("yes you are karthick")
+#         print("welcome")
+#     print("skipped above if due to name mismatch")   
+#     for x in name:
+#         print(x)  
+# myname()
+
+#use of function
+# to reuse the function again and again
+# def myname1():
+#     print("I am karthick AG")
+# myname1()
+# myname1()
+# myname1()
+
+
+
+# def myname2():
+#     name="karthick"
+#     lname="AG"
+#     year=2024
+#     # printing value as it is.
+#     print("hi..!" , name)
+#     print("hi..!" , name , lname)
+#     print("hi..!" , name , lname , "welcome to year of ", year)
+
+#     # printing value using concatenation
+#     print("--------")
+#     print("printing value using concatenation")
+#     print("hi..!" + name)
+#     print("hi..!" + name + " " + lname)
+#     print("hi..!" + name + " " + lname + "welcome to year of " + str(year))
+
+# myname2()
+
+def myname2(name, lname, year):
+    # printing value as it is.
+    print("hi..!" , name)
+    print("hi..!" , name , lname)
+    print("hi..!" , name , lname , "welcome to year of ", year)
+
+    # printing value using concatenation
+    print("--------")
+    print("printing value using concatenation")
+    print("hi..!" + name)
+    print("hi..!" + name + " " + lname)
+    print("hi..!" + name + " " + lname + "welcome to year of " + str(year))
+
+myname2("karthick","AG",2024)
+name="Dhanush"
+lname="Raguvaran"
+year=2024
+myname2(name,lname,year)
+
